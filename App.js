b76bb1eab7aeb118d61cad27cadd8c31316f4a16
aadamcz <33779500+aadamcz@users.noreply.github.com>
2018-01-06 15:30:27 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
+var contacts = [
+	{
+		id: 1,
+		firstName: "Jan",
+		lastName: "Nowak",
+		email: "jan.nowak@example.com"
+	},
+	{
+		id: 2,
+		firstName: "Adam",
+		lastName: "Kowalski",
+		email: "adam.kowalski@example.com"
+	},
+	{
+		id: 3,
+		firstName: "Zbigniew",
+		lastName: "Koziol",
+		email: "zbigniew.koziol@example.com"
+	}
+];
+
 var App = React.createClass({
 	render: function() {
 		return (
